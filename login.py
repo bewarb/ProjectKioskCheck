5b9eada563121a8eb80741c90e88c50659e7f561
Ismael Sanchez <105394824+SmishyFamlit@users.noreply.github.com>
2023-12-13 18:14:49 -0500

    Add a hashed password feature & redirect works

--- login.py
+++ login.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, render_template
 from datetime import datetime
 import csv
+import hashlib
+
+# Hash the password using a SHA-256
+def hash_password(password):
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    return hashed_password
 
 app = Flask(__name__)
 app.debug = True
@@ -9,29 +15,28 @@ app.debug = True
 def index():
     return render_template('login.html')
 
-# @app.route('/form', methods=['POST'])
-# def form():
-#     return render_template('printer-page.html')
+
 
 @app.route('/submit', methods=['POST'])
 def submit():
     name = request.form.get('name')
     email = request.form.get('email')
-    user_passcode = request.form.get('passcode')
+    user_passcode = hash_password(request.form.get('passcode'))
 
     # Save the data to a CSV file
     with open('data.csv', 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow([name, email, datetime.now().strftime('%m-%d-%Y %H:%M')[0:10], datetime.now().strftime('%m-%d-%Y %H:%M')[11:16]])
-    file = csv.reader(open("kioskers.csv"))
+    file = csv.reader(open("user_credentials.csv"))
     for name, password in file:
-        if password != user_passcode:
-            # return render_template("printer-page.html")
-            # return printer_page()
-            return "Incorrect Password"
+        if password == user_passcode:
+            return render_template("printer-page.html")
 
-    # return "Incorrect Passcode"
+    return "Incorrect password"
 
+@app.route('/submit2', methods=['POST'])
+def submit2():
+    return 'This is the next page'
 
 if __name__ == '__main__':
     app.run()
